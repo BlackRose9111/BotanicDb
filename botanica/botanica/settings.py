afb89966322b0afde339aa43b6a5b7e0480a9208
Black Rose <86892804+BlackRose9111@users.noreply.github.com>
2024-01-23 00:28:42 +0300

    Major project setup and stuff

--- botanica/botanica/settings.py
+++ botanica/botanica/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                "django.template.context_processors.media",
             ],
         },
     },
@@ -126,7 +127,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = 'media/'
+#MEDIA_URL = 'media/'
 
 
 
--- botanica/home/models.py
+++ botanica/home/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
 # Create your models here.
+
+class Plant(models.Model):
+
+    class Meta:
+        verbose_name = "Bitki"
+        verbose_name_plural = "Bitkiler"
+
+
+    class resistance(models.TextChoices):
+        LOW = "Düşük"
+        MEDIUM = "Orta"
+        HIGH = "Yüksek"
+        UNKNOWN = "Bilinmiyor"
+
+    class woody_type(models.TextChoices):
+        woody = "Odunsu"
+        herbaceous = "Otsu"
+        unknown = "Bilinmiyor"
+
+    class plan_types(models.TextChoices):
+        main_plant = "Ana Bitki"
+        under_the_pot = "Saksı Altı"
+        no_plan = "Plan Yok"
+
+    common_name = models.CharField(max_length=1000,blank=True, null=True, unique=True,verbose_name="Bilinen Adı")
+    scientific_name = models.CharField(max_length=1000,blank=True, null=True, unique=True,verbose_name="Bilimsel Adı")
+    description = models.TextField(blank=True, null=True,verbose_name="Açıklama")
+    most_fertile_type = models.CharField(max_length=1000,blank=True, null=True,verbose_name="En Verimli Türü")
+    bonsai = models.BooleanField(default=False,verbose_name="Bonsai İçin Uygun")
+    available_in_turkey = models.BooleanField(default=False,verbose_name="Türkiye'de Bulunabilir")
+    maximum_height = models.IntegerField(null=True,verbose_name="Max Yükseklik")
+    min_temperature_resistance = models.IntegerField(null=True,verbose_name="Min Sıcaklık Direnci")
+    max_temperature_resistance = models.IntegerField(null=True,verbose_name="Max Sıcaklık Direnci")
+    arid_resistance = models.CharField(max_length=10, choices=resistance.choices, default=resistance.UNKNOWN,verbose_name="Kuraklığa Direnç")
+    overwater_resistance = models.CharField(max_length=10, choices=resistance.choices, default=resistance.UNKNOWN,verbose_name="Aşırı Sulamaya Direnç")
+    reproduction_with_single_plant_autogamy = models.BooleanField(default=False,verbose_name="Tek Bitki ile Üreme (Otogami)")
+    reproduction_by_wind_anemogamy = models.BooleanField(default=False,verbose_name="Rüzgar ile Tozlaşam (Anemogami)")
+    edible_fruit = models.BooleanField(default=False, null=True,verbose_name="Yenilebilir Meyve")
+    harmful_to_cats = models.BooleanField(default=False, null=True,verbose_name="Kediler İçin Zararlı")
+    hours_of_sunlight = models.IntegerField(null=True,verbose_name="Kaç Saat Güneş Işığı Alabilir",default=0)
+    litres_of_soil = models.IntegerField(null=True,verbose_name="Kaç Litre Toprak İhtiyacı Var",default=0)
+    woody_type = models.CharField(max_length=10, choices=woody_type.choices, default=woody_type.unknown,verbose_name="Odunsu/Otsu(Herbaceous)")
+    yearly_shoots = models.BooleanField(default=False,verbose_name="Her Yıl Yeni Sürgün")
+    plan = models.CharField(max_length=10, choices=plan_types.choices, default=plan_types.no_plan,verbose_name="Plan")
+    updated_at = models.DateTimeField(auto_now=True,verbose_name="Güncelleme Tarihi")
+    created_at = models.DateTimeField(auto_now_add=True,verbose_name="Oluşturulma Tarihi",null=True)
+    image = models.ImageField(upload_to="media/", blank=True, null=True,verbose_name="Resim")
+
+    def __str__(self):
+        return f"{self.common_name} ({self.scientific_name})"
+
+
+
+
+
