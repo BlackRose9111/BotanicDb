fe5af179cfa9a75cbb742d034a5c8618245ea1d6
Black Rose <86892804+BlackRose9111@users.noreply.github.com>
2024-01-24 12:06:34 +0300

    Bootstrap

--- botanica/home/views.py
+++ botanica/home/views.py
@@ -8,16 +8,8 @@ from random import randint
 def index(request):
 
     #query the database for a plant of the day for today, if there is not one create one for today by randomly selecting a plant
-    plant_of_the_day = models.PlantOfTheDay.objects.filter(date__date=datetime.today()).first()
-    if not plant_of_the_day:
-        plant = list(models.Plant.objects.all())
-        length = len(plant)
-        if length > 0:
-            random_plant = plant[randint(0,length-1)]
-            plant_of_the_day = models.PlantOfTheDay.objects.create(plant=random_plant,date=datetime.today())
-            plant_of_the_day.save()
 
-    return render(request, "home/index.html", {"plant_of_the_day":plant_of_the_day})
+    return render(request, "home/index.html", {})
 
 
 def plant(request, plant_id : int ):
