d4fa2672069754b862feeceb6e7afe4c4cd8ba7b
Black Rose <86892804+BlackRose9111@users.noreply.github.com>
2024-01-25 12:23:13 +0300

    I have no idea why the items on the navbar just behave so stupid

--- botanica/home/urls.py
+++ botanica/home/urls.py
@@ -7,5 +7,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("plant/<int:plant_id>", views.plant, name="plant"),
+    path("logout", views.logout_view, name="logout"),
+    path("login", views.login_view, name="login"),
+    path("register", views.register, name="register"),
 ]
 
--- botanica/home/views.py
+++ botanica/home/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
-from datetime import datetime
-from random import randint
-# Create your views here.
-
+from django.contrib.auth import logout
 
 def index(request):
 
-    #query the database for a plant of the day for today, if there is not one create one for today by randomly selecting a plant
 
     return render(request, "home/index.html", {})
 
@@ -15,3 +11,15 @@ def index(request):
 def plant(request, plant_id : int ):
     plant = models.Plant.objects.get(id=plant_id)
     return render(request, "home/plant.html", {"plant":plant})
+
+
+def logout_view(request):
+    #logout the user and then render the homepage
+    logout(request)
+    return redirect('home')
+
+def login_view(request):
+    #redirect to the admin login page for now
+    return redirect('admin:index')
+def register(request):
+    return "Hello world"
